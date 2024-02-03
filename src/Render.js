7a58d5f00f2a2ac2419372ec7649085a412372b6
resetmerlin <resetmerlin@gmail.com>
2024-02-03 18:02:49 +0900

    feat: Add Render

---
+++ src/Render.js
@@ -0,0 +1,18 @@
+export const render = (component) => {
+  if (typeof component === "string") {
+    return document.createTextNode(component);
+  }
+  const { type, attr, children } = component;
+
+  const $component = document.createElement(type);
+
+  for (const [key, value] of Object.entries(attr)) {
+    $component.setAttribute(key, value);
+  }
+
+  children.forEach((child) => {
+    $component.appendChild(render(child));
+  });
+
+  return $component;
+};
