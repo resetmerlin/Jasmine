ff0395b2e43a64ab21cf000e4ad5288df19be51d
resetmerlin <resetmerlin@gmail.com>
2024-02-07 16:21:46 +0900

    feat: Add core jamine

---
+++ core/Jasmine.js
@@ -0,0 +1,60 @@
+const Jasmine = {
+  init(container, oldNode, newNode) {
+    container.appendChild(oldNode);
+    this.patch(oldNode, newNode);
+  },
+
+  patch(oldNode, newNode) {
+    if (!newNode) return;
+
+    const renderedNewNode = this.render(newNode);
+
+    if (oldNode.isEqualNode(renderedNewNode)) return;
+
+    if (oldNode.nodeName !== renderedNewNode.nodeName) {
+      oldNode.parentNode.replaceChild(renderedNewNode, oldNode);
+    }
+
+    if (
+      oldNode &&
+      renderedNewNode &&
+      oldNode.attributes.length !== renderedNewNode.attributes.length
+    ) {
+      for (const attr of renderedNewNode.attributes) {
+        oldNode.setAttribute(attr.name, attr.value);
+      }
+    }
+
+    if (oldNode.childNodes.length !== renderedNewNode.childNodes.length) {
+      oldNode.parentNode.replaceChild(renderedNewNode, oldNode);
+    } else {
+      for (let i = 0; i < oldNode.childNodes.length; i++) {
+        this.patch(oldNode.childNodes[i], newNode.children[i]);
+      }
+    }
+  },
+
+  render(component) {
+    if (!component) return;
+
+    if (typeof component === "string") {
+      return document.createTextNode(component);
+    }
+
+    const { type, attr, children } = component;
+
+    const $component = document.createElement(type);
+
+    for (const [key, value] of Object.entries(attr)) {
+      $component.setAttribute(key, value);
+    }
+
+    children.forEach((child) => {
+      $component.appendChild(this.render(child));
+    });
+
+    return $component;
+  },
+};
+
+export default Jasmine;
