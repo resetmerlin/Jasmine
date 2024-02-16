69eb6192bb49f6e6623eef1b2fdd4773ff5d3b15
resetmerlin <resetmerlin@gmail.com>
2024-02-16 14:02:58 +0900

    refactor: Delete unecessary code

--- src/CreateElement.js
+++
@@ -1,5 +0,0 @@
-export const createElement = (type, attr, ...children) => {
-  if (!attr) attr = {};
-
-  return { type, attr, children };
-};
--- src/Init.js
+++
@@ -1,6 +0,0 @@
-import { patch } from "./Patch";
-
-export const init = (container, oldNode, newNode) => {
-  container.appendChild(oldNode);
-  patch(oldNode, newNode);
-};
